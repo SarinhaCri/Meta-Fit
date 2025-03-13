3ca4f1a865ced2531b0c697616e7eff565dce637
Sara Cristina <sara.crys2002@gmail.com>
2025-03-13 15:52:48 -0300

    Bem-vindo ao Meta Fit, um site dedicado a auxiliar você na sua jornada fitness!

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btnCardapio").addEventListener("click", function () {
+        window.location.href = "cardapio.html";
+    });
+
+    document.getElementById("btnTreinos").addEventListener("click", function () {
+        window.location.href = "treino.html";
+    });
+});
